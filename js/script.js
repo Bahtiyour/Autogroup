b3311458a511a67f1ce9dc44094d1c33cca70888
Bahtiyour <cinno162@gmail.com>
2023-03-10 19:01:06 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  // Нажатие на бургер  
+  $(".burger__header").click(function (event) {
+    $(".burger__header,.header__group").toggleClass("show");
+  });
+  // Маска для номера
+  const phoneInput = document.querySelector(".phone");
+  $(".phone").mask("+375 (99) 99-99-999"), { placeholder: "mm/dd/yyyy" };
+});
+// Отправка формы на js
+$("#send_button").click(function (e) {
+  // console.log( $( "input[name~='name']" ).val()),
+  $.ajax({
+    type: "POST",
+    url: "saindmail.php", //Не уверен
+    data: {
+      name: $("input[name~='name']").val(),
+      phone:  $(".phone").val(),
+      city: $("input[name~='city']").val(),
+    },
+    success: function (data) {
+      console.log(data.data);
+    },
+  });
+  // Проверка на заполнение
+  $(document).ready(function(){
+    $("#demoForm").validate();
+  });
+  $.validator.messages.required = "Заполните поле!";
+  $.validator.messages.email = "Неверный формат email";
+  $.validator.messages.url = "Неверный формат url,";
+});
