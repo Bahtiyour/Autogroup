ec5817e21f6d3d03ea5896bd617a07057bb3dd01
Bahtiyour <cinno162@gmail.com>
2023-03-23 10:15:58 +0300

    123

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  // Нажатие на бургер  
+  // Нажатие на бургер
   $(".burger__header").click(function (event) {
     $(".burger__header,.header__group").toggleClass("show");
   });
@@ -8,36 +8,38 @@ $(document).ready(function () {
   $(".phone").mask("+375 (99) 99-99-999"), { placeholder: "mm/dd/yyyy" };
 });
 // Отправка формы на js
-$("#send_button").click (function (e) {
-    var form = $(this.closest("form"));
-    var field = [];
-    form.find('input.required').each(function () {
-      field.push('input.required');
-      var value = $(this).val(),
-          line = $(this);
-      for(var i=0;i<field.length;i++) {
-        if( !value ) {
-          line.addClass('feedback__group-required');
-          setTimeout(function() {
-            line.removeClass('feedback__group-required')
-          }.bind(this),2000);
-          event.preventDefault();
-        }
+$("#send_button").click(function (e) {
+  var form = $(this.closest("form"));
+  var field = [];
+  form.find("input.required").each(function () {
+    field.push("input.required");
+    var value = $(this).val(),
+      line = $(this);
+    for (var i = 0; i < field.length; i++) {
+      if (!value) {
+        line.addClass("feedback__group-required");
+        setTimeout(
+          function () {
+            line.removeClass("feedback__group-required");
+          }.bind(this),
+          2000
+        );
+        event.preventDefault();
       }
-    });
+    }
+  });
   // console.log( $( "input[name~='name']" ).val()),
   $.ajax({
     type: "POST",
     url: "saindmail.php", //Не уверен это не точно
     data: {
       name: $("input[name~='name']").val(),
-      phone:  $(".phone").val(),
+      phone: $(".phone").val(),
       city: $("input[name~='city']").val(),
     },
     success: function (data) {
       console.log(data.data);
     },
   });
-  });
-// Email  
-
+});
+//
--- php/saindmail.php
+++ php/saindmail.php
@@ -1,3 +1,25 @@
 <?php
-    if($)
+    if($Post['feedback'] == 1){
+        $feedback = 'Имя';
+    }
+    elseif($Post['feedback'] == 2){
+        $feedback = 'Телефон';
+    }
+    elseif($Post['feedback'] == 3){
+        $feedback = 'Город';
+    }
+    elseif($Post['feedback'] == 4){
+        $feedback = 'E-Mail';
+    } else{
+        $feedback = 'Ничего не заполнено:(';
+    }
+
+    $to = "cinno162@gmail.com";
+    $from = trim$Post['email'];
+
+    if(mail($to, $feedback)){
+        echo 'Ваше письмо доставлено!'
+    } else{
+         echo 'Ваше письмо не доставлено:('
+    }
 ?>
