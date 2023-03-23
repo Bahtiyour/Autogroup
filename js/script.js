1b21d93329c7ceb8e54dacaa9e93cb88c9fc7a2c
Bahtiyour <cinno162@gmail.com>
2023-03-23 18:17:55 +0300

    123

--- js/script.js
+++ js/script.js
@@ -4,42 +4,48 @@ $(document).ready(function () {
     $(".burger__header,.header__group").toggleClass("show");
   });
   // Маска для номера
-  const phoneInput = document.querySelector(".phone");
-  $(".phone").mask("+375 (99) 99-99-999"), { placeholder: "mm/dd/yyyy" };
-});
-// Отправка формы на js
-$("#send_button").click(function (e) {
-  var form = $(this.closest("form"));
-  var field = [];
-  form.find("input.required").each(function () {
-    field.push("input.required");
-    var value = $(this).val(),
-      line = $(this);
-    for (var i = 0; i < field.length; i++) {
-      if (!value) {
-        line.addClass("feedback__group-required");
-        setTimeout(
-          function () {
-            line.removeClass("feedback__group-required");
-          }.bind(this),
-          2000
-        );
-        event.preventDefault();
-      }
-    }
+  $(".phone").mask("+375 (99) 99-99-999");
+  // Проверка на заполненность
+  $("#send_button").click(function (e) {
+    let form = this.closest("form");
+    var field = [];
+    $(form)
+      .find("input.required")
+      .each(function () {
+        field.push("input.required");
+        var value = $(this).val(),
+          line = $(this);
+        for (var i = 0; i < field.length; i++) {
+          if (!value) {
+            line.addClass("feedback__group-required");
+            setTimeout(
+              function () {
+                line.removeClass("feedback__group-required");
+              }.bind(this),
+              2000
+            );
+            event.preventDefault();
+          }
+        }
+      });
+    // console.log( $( "input[name~='name']" ).val()),
+    // Отправка формы
+    let error = (error);
+
+    if error( = 0 ){
+      let formData = new FormData(form)
+      console.log()
+      $.ajax({
+        url: "/php/saindmail.php", //Не уверен это не точно
+        method: "POST",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+          console.log(data.data);
+        },
+      });
+    };
   });
-  // console.log( $( "input[name~='name']" ).val()),
-  $.ajax({
-    type: "POST",
-    url: "saindmail.php", //Не уверен это не точно
-    data: {
-      name: $("input[name~='name']").val(),
-      phone: $(".phone").val(),
-      city: $("input[name~='city']").val(),
-    },
-    success: function (data) {
-      console.log(data.data);
-    },
-  });
+  //
 });
-//
--- php/saindmail.php
+++ php/saindmail.php
@@ -1,25 +1,11 @@
 <?php
-    if($Post['feedback'] == 1){
-        $feedback = 'Имя';
-    }
-    elseif($Post['feedback'] == 2){
-        $feedback = 'Телефон';
-    }
-    elseif($Post['feedback'] == 3){
-        $feedback = 'Город';
-    }
-    elseif($Post['feedback'] == 4){
-        $feedback = 'E-Mail';
-    } else{
-        $feedback = 'Ничего не заполнено:(';
-    }
+
 
     $to = "cinno162@gmail.com";
-    $from = trim$Post['email'];
-
+    $from = trim($POST)['email'];
     if(mail($to, $feedback)){
         echo 'Ваше письмо доставлено!'
     } else{
-         echo 'Ваше письмо не доставлено:('
+        echo 'Ваше письмо не доставлено:('
     }
 ?>
